f70bbb5de9167b9503186b90f85dde78e0e03b0c
Lolo1710 <maattos07@gmail.com>
2025-11-14 08:50:56 -0500

    v2.1.5

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const db = mysql.createPool({
     host: "174.136.28.105",       // Cambia a tu host
     user: "cvjajcco_alan",            // Usuario de tu DB
-    password: "jajajojo2025.",            // Contraseña de tu DB
+    password: "madisonwildeputa.",            // Contraseña de tu DB
     database: "cvjajcco_lavanderia",  // Nombre de tu DB
     waitForConnections: true,
     connectionLimit: 10,
