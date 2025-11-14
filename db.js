e4d5c09f2a830b9d34febdcffaa1021e112b9540
Lolo1710 <maattos07@gmail.com>
2025-11-14 09:05:51 -0500

    v2.1.9

--- db.js
+++ db.js
@@ -4,10 +4,8 @@ const db = mysql.createPool({
     host: "61.247.177.142",       // Cambia a tu host
     user: "cvjajcco_alan",            // Usuario de tu DB
     password: "AlanUriel2026.!",            // Contraseña de tu DB
-    database: "cvjajcco_lavanderia",  // Nombre de tu DB
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    database: "cvjajcco_lavanderia",
+    port: 3306,
 });
 
 export { db };
